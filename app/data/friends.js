2ae0ec737bb00ae3cea3abb1a441c3c7b2d79181
teenarr <tina.chan91@gmail.com>
2019-01-02 23:21:06 -0700

    got form functioning and apiRoutes to post user's match to a modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
   {
-    "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name": "Arnold",
+    "photo": "http://lorempixel.com/400/200/people",
     "scores": [
       5,
       1,
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,18 +4,38 @@ var friendData = require("../data/friends");
 // ROUTING
 // ===============================================================================
 
-module.exports = function(app) {
+module.exports = function (app) {
 
-  app.get("/api/friends", function(req, res) {
+  app.get("/api/friends", function (req, res) {
     return res.json(friendData);
   });
 
-  app.post("/api/friends", function(req, res) {
+  app.post("/api/friends", function (req, res) {
     console.log(req.body)
-    friendData.push(req.body);
-  });
+    var newFriend = req.body;
 
-  // ---------------------------------------------------------------------------
-  // I added this below code so you could clear out the table while working with the functionality.
-  // Don"t worry about it!
+    console.log(friendData)
+    
+    var friendMatch = "";
+    var matchDifference = 10000;
+
+    for (var i = 0; i < friendData.length; i++) {
+      friendDifference = 0;
+
+      for (var j = 0; j < friendData[i].scores.length; j++) {
+        friendDifference += Math.abs(friendData[i].scores[j] - newFriend.scores[j]);
+      }
+      console.log(friendData[i].name + " difference: " + friendDifference);
+
+      if (friendDifference < matchDifference) {
+        matchDifference = friendDifference;
+        friendMatch = i;
+      }
+    }
+
+    console.log(friendMatch);
+    
+    friendData.push(newFriend);
+    res.json(friendData[friendMatch]);
+  });
 };
