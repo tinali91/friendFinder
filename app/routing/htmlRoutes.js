6634e90465698e243f292882c64931e8b0defbe4
teenarr <tina.chan91@gmail.com>
2019-01-02 23:39:34 -0700

    fixed some routing issues

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,6 +5,10 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"))
+  })
+
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"))
   })
