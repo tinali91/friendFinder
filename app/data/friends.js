9e875bfaed1158c5a330cf7c68ba7ba63efc156f
teenarr <tina.chan91@gmail.com>
2019-01-21 22:42:59 -0700

    added more people into the friends data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,12 +1,44 @@
 var friends = [
   {
     "name": "Arnold",
-    "photo": "http://lorempixel.com/400/200/people",
+    "photo": "https://images.unsplash.com/photo-1508341591423-4347099e1f19?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
     "scores": [
       5,
       1,
       4,
       4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+  },
+  {
+    "name": "Cher",
+    "photo": "https://www.irishtimes.com/polopoly_fs/1.3619387.1536162280!/image/image.jpg_gen/derivatives/box_620_330/image.jpg",
+    "scores": [
+      1,
+      2,
+      3,
+      4,
+      5,
+      5,
+      4,
+      3,
+      2,
+      1
+    ]
+  },
+  {
+    "name": "Jasmine",
+    "photo": "http://pm1.narvii.com/6378/ad261bfa56b70f6e8cbf0930892817e1d37e00b7_00.jpg",
+    "scores": [
+      4,
+      5,
+      4,
+      3,
       5,
       1,
       2,
